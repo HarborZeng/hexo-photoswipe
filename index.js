97624491b54faf611f01fb24639fc080449d0521
harbo <757772438@qq.com>
2019-02-11 20:44:23 +0800

    update priority

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const renderImageResolution = require('./util/image-res-renderer');
 hexo.extend.filter.register('after_post_render', function (data) {
     data.content = renderImageResolution(data.content, data.title)
     return data
-})
+}, 15)
