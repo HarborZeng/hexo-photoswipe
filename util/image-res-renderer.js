1e43a1ff5bab95fb3c7801743f89c69e75ddd565
harbo <757772438@qq.com>
2019-02-12 21:21:09 +0800

    ...

--- util/image-res-renderer.js
+++ util/image-res-renderer.js
@@ -2,7 +2,7 @@ const sizeOf = require('image-size');
 const path = require('path');
 
 function renderImageResolution(content, title) {
-  return content.replace(/(<img src="(.*)" data-original="([^"]*)".*>)/g, function (match, p1, p2, p3, offset, string) {
+  return content.replace(/(<img src="(.*)" data-original="([^"]*)"[^/p]*>)/g, function (match, p1, p2, p3, offset, string) {
     title = title.replace(/[,_:'";?!@><.]/gi, '-').replace(/ /g, '')
     var img = p3.replace(/(\/p\/)(.*)(\/.*)/g, title + '$3')
     img = path.join("source/_posts", img)
