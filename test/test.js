eb36f026827e245ba04ade85e95a6c0e1e38bef0
harbo <757772438@qq.com>
2019-05-07 17:28:49 +0800

    update test

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
-const assert  = require("assert");
+const assert = require("assert");
 const renderImageResolution = require('../util/image-res-renderer');
-hexo = {
+var hexo = hexo || {
   config: {
     photoswipe: {
       enable: true,
@@ -12,9 +12,9 @@ hexo = {
   }
 }
 let localImageTestCase = '<img src="/medias/assets/loading/Eclipse-1s-200px.svg" data-original="/p/machine-learning-tensorflow-keras-predicts-car-fuel-efficiency-the-regression-problem/1550060359264.png" title="数据有规律了许多">'
-let resultLocal = renderImageResolution(localImageTestCase, "title1");
+let resultLocal = renderImageResolution(localImageTestCase, "title1", hexo.config);
 assert.equal(resultLocal, '<div class="image-container" data-type="content-image" data-size="533x532"><img src="/medias/assets/loading/Eclipse-1s-200px.svg" data-original="/p/machine-learning-tensorflow-keras-predicts-car-fuel-efficiency-the-regression-problem/1550060359264.png" title="数据有规律了许多"></div>')
 
 let networkImageTestCase = '<img src="/medias/assets/loading/Eclipse-1s-200px.svg" data-original="https://upload-images.jianshu.io/upload_images/6399013-3d0ee36e36870916.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240" alt="">'
-let resultNetwork = renderImageResolution(networkImageTestCase, "title2");
+let resultNetwork = renderImageResolution(networkImageTestCase, "title2", hexo.config);
 assert.equal(resultNetwork, '<div class="image-container" data-type="content-image" data-size="960x511"><img src="/medias/assets/loading/Eclipse-1s-200px.svg" data-original="https://upload-images.jianshu.io/upload_images/6399013-3d0ee36e36870916.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240" alt=""></div>')
