79e38bbc3e24d037f77539abe2307fa99fea8c7a
harbo <757772438@qq.com>
2019-05-07 17:04:38 +0800

    fix: config bug

--- util/image-res-renderer.js
+++ util/image-res-renderer.js
@@ -4,7 +4,7 @@ const syncRequest = require('sync-request');
 const fs = require('hexo-fs');
 const imageCachePath = './img-cache/'
 
-function renderImageResolution(content, title) {
+function renderImageResolution(content, title, config) {
   var imageMap = new Map()
   // 一行一行读取image-size.db内容，不存在就创建之
   fs.writeFileSync(path.join(imageCachePath, 'image-size.db'), '', {"flag": 'a+'})
@@ -15,10 +15,10 @@ function renderImageResolution(content, title) {
 
   let imgTagRegExp_dataOriginal = /(<img src="(.*)" data-original="([^"]*)"[^<]*>)/g
   let imgTagRegExp_src = /(<(img) src="([^"]*)"[^<]*>)/g
-  return content.replace(hexo.config.photoswipe.imgSrcIn === 'dataOriginal' ? imgTagRegExp_dataOriginal : imgTagRegExp_src, function (match, p1, p2, p3, offset, string) {
+  return content.replace(config.photoswipe.imgSrcIn === 'dataOriginal' ? imgTagRegExp_dataOriginal : imgTagRegExp_src, function (match, p1, p2, p3, offset, string) {
     title = title.replace(/[,_:'";?!@><.]/gi, '-').replace(/ /g, '')
     var img = p3.replace(/\/.*\/([^"]*)/g, path.join(title, '$1'))
-    img = path.join(hexo.config.photoswipe.imageFileBaseDir, img)
+    img = path.join(config.photoswipe.imageFileBaseDir, img)
     var imageSize = {}
     if (p3.indexOf('http') === 0) {
       // 以http开头的data-origianl属性
@@ -69,7 +69,7 @@ function renderImageResolution(content, title) {
       // 本地图片，可以求取大小
       imageSize = sizeOf(img)
     }
-    return '<div class="' + hexo.config.photoswipe.className + '" data-type="' + hexo.config.photoswipe.dataType + '" data-size="' + imageSize.width + 'x' + imageSize.height + '">' + p1 + '</div>'
+    return '<div class="' + config.photoswipe.className + '" data-type="' + config.photoswipe.dataType + '" data-size="' + imageSize.width + 'x' + imageSize.height + '">' + p1 + '</div>'
   })
 }
 
